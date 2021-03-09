e279f636c05e678e27ec22026b58a79e5e395240
Patrick Sodré <psodre@gmail.com>
2021-03-09 13:56:12 -0500

    Update src/main/java/co/zeroae/gate/AppMetadata.java

--- src/main/java/co/zeroae/gate/AppMetadata.java
+++ src/main/java/co/zeroae/gate/AppMetadata.java
@@ -1,5 +1,7 @@
 package co.zeroae.gate;
-
+/**
+ * This class structure must match https://github.com/GateNLP/cloud-client/blob/master/library/src/main/java/uk/ac/gate/cloud/online/ServiceMetadata.java
+ */
 class AppMetadata {
     public String name;
 
